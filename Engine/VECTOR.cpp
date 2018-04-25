0752625c7a0ecd894e4f07e38adc7b49ec9bf692
Tausif <anodecode@gmail.com>
2018-04-25 10:08:56 +0530

    added barebones files and implemented Vector

---
+++ Engine/VECTOR.cpp
@@ -0,0 +1,64 @@
+#include "VECTOR.h"
+
+#include <cmath>
+
+double VECTOR2D::calculateDist()
+{
+	return std::sqrt(m_Vx*m_Vx+m_Vy*m_Vy);
+}
+
+VECTOR2D VECTOR2D::operator+(const VECTOR2D & vec) const
+{
+	return VECTOR2D((m_Vx + vec.m_Vx), (m_Vy + vec.m_Vy));
+}
+
+VECTOR2D VECTOR2D::operator-(const VECTOR2D & vec) const
+{
+	return VECTOR2D((m_Vx - vec.m_Vx), (m_Vy - vec.m_Vy));
+}
+
+VECTOR2D VECTOR2D::operator*(const double & scale) const
+{
+	return VECTOR2D((m_Vx*scale),(m_Vy*scale));
+}
+
+VECTOR2D VECTOR2D::operator/(const double & scale) const
+{
+	return VECTOR2D((m_Vx/scale), (m_Vy/scale));
+}
+
+VECTOR2D VECTOR2D::Dot(const VECTOR2D & a, const VECTOR2D & b) const
+{
+	return VECTOR2D((a.m_Vx * b.m_Vx), (a.m_Vy * b.m_Vy));
+}
+
+double VECTOR2D::getX() const
+{
+	return m_Vx;
+}
+
+double VECTOR2D::getY() const
+{
+	return m_Vy;
+}
+
+double VECTOR2D::getDistance() const
+{
+	return dist;
+}
+
+VECTOR2D VECTOR2D::getUnitVector() const
+{
+	return ((*this)/(dist));
+}
+
+VECTOR2D::VECTOR2D(const double& VX, const double& VY)
+{
+	m_Vx = VX;
+	m_Vy = VY;
+	dist = calculateDist();
+}
+
+VECTOR2D::~VECTOR2D()
+{
+}
---
+++ Engine/VECTOR.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+class VECTOR2D
+{
+private:
+	double m_Vx;
+	double m_Vy;
+
+	double dist;
+
+	double calculateDist();
+
+public:
+	VECTOR2D operator+(const VECTOR2D & vec) const;
+	VECTOR2D operator-(const VECTOR2D & vec) const;
+	VECTOR2D operator*(const double & scale) const;
+	VECTOR2D operator/(const double & scale) const;
+	VECTOR2D & operator=(const VECTOR2D & vec) = default;
+	
+	VECTOR2D Dot(const VECTOR2D & vecA, const VECTOR2D & vecB) const;
+
+	double getX() const ;
+	double getY() const ;
+	double getDistance() const;
+	VECTOR2D getUnitVector() const;
+
+	VECTOR2D() = delete;
+	VECTOR2D(const VECTOR2D &) = default;
+	VECTOR2D(const double &, const double &);
+	~VECTOR2D();
+};
+
